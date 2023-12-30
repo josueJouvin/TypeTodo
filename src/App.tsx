a6c1f50269059b91fd1d84ffdb34dd78743e460d
josue jouvin <josuejouvin27@gmail.com>
2023-12-29 22:54:16 -0500

    Todo Typescript

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import { Todos } from './components/Todos'
+import { type TodoId } from './types'
+
+const mockTodos = [
+  {
+    id: '1',
+    title: 'todo 1',
+    completed: false
+  },
+  {
+    id: '2',
+    title: 'todo 2',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'todo 3',
+    completed: false
+  }
+]
+
+const App = (): JSX.Element => {
+  const [todos, setTodos] = useState(mockTodos)
+
+  const handleRemove = ({ id }: TodoId): void => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+
+  return (
+    <div className="todoapp">
+      <Todos
+        todos={todos}
+        onRemoveTodo={handleRemove}
+      />
+    </div>
+  )
+}
+
+export default App
